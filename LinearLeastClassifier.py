7114ab1436f6705b1d64c8c3a85b85f67b2f1c34
Trevor Yovaish <tyovaish13@gmail.com>
2017-04-25 15:10:03 -0400

    Working on our shit

---
+++ LinearLeastClassifier.py
@@ -0,0 +1,14 @@
+
+def train(trainingSet,trainingValidation):
+
+    return weightMatrix
+
+
+
+def test(weightMatrix,testSet):
+
+
+def crossValidation(weightMatrix,):
+
+
+def createConfusionMatrix():
