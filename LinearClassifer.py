20cf74359f95a49866e7d88a63d0f52f02c56452
Tyovaish <tyovaish13@gmail.com>
2017-04-25 12:38:48 -0700

    Added structure in LinearClassifer

---
+++ LinearClassifer.py
@@ -0,0 +1,6 @@
+def train(trainingSet,trainingValidation):
+    return 0
+def test(weightMatrix,testSet,testValidation):
+    return 0
+def crossValidation(weightMatrix,testSet,testValidation):
+    return 0
